3ad77019137445a0cfe1211e27229508945cb2e7
NushratNowrin <nowrin2023@gmail.com>
2023-11-07 00:48:51 +0600

    string

--- module-1/src/index.ts
+++ module-1/src/index.ts
@@ -1,2 +1,13 @@
 const course : string = "Next level web development";
 console.log(course);
+
+// Basic Data Type
+// -----------------------------------------------------------------------
+// String
+// Implicit Data Type
+let firstName = "Nushrat";
+
+// Explicit Data Type
+let firstName2 : string = "Nushrat";
+
+// Number
