08bacf3e48d86e0f1365a39ddafe989e68bc9474
NushratNowrin <nowrin2023@gmail.com>
2023-11-08 00:48:22 +0600

    normal function

--- module-1/dist/index.js
+++ module-1/dist/index.js
@@ -1,33 +1,47 @@
 "use strict";
-const course = "Next level web development";
-console.log(course);
-// Basic Data Type
-// -----------------------------------------------------------------------
-// String
-// Implicit Data Type
-let firstName = "Nushrat";
-// Explicit Data Type
-let firstName2 = "Nushrat";
-// Number
-let roll = 1718048;
-// Boolean
-let isGraduated = true;
-// null
-let nothing = null;
-// undefined
-let bolahoyni = undefined;
-// any
-let d;
-d = 1;
-d = 'abc';
-d = true;
-d = undefined;
-d = null;
-console.log(d);
-// Array
-let friends = ['Hasib', 'Hasan'];
-let rolls = [1, 3];
-// Tuple
-let ageName = [20, "Mr. X"];
-ageName[1] = 2;
-console.log(ageName);
+{
+    const course = "Next level web development";
+    console.log(course);
+    // Basic Data Type
+    // -----------------------------------------------------------------------
+    // String
+    // Implicit Data Type
+    let firstName = "Nushrat";
+    // Explicit Data Type
+    let firstName2 = "Nushrat";
+    // Number
+    let roll = 1718048;
+    // Boolean
+    let isGraduated = true;
+    // null
+    let nothing = null;
+    // undefined
+    let bolahoyni = undefined;
+    // any
+    let d;
+    d = 1;
+    d = "abc";
+    d = true;
+    d = undefined;
+    d = null;
+    console.log(d);
+    // Array
+    let friends = ["Hasib", "Hasan"];
+    let rolls = [1, 3];
+    // Tuple
+    let ageName = [20, "Mr. X", true];
+    // Object
+    const user = {
+        company: "Programming Hero",
+        firstName: "Nushrat",
+        middleName: "Jahan",
+        Husband: "Hasan Ali",
+    };
+    // Function
+    // Normal Function
+    function add(num1, num2) {
+        return num1 + num2;
+    }
+    const addNumber = add(2, 3);
+    console.log(addNumber);
+}
--- module-1/src/index.ts
+++ module-1/src/index.ts
@@ -1,60 +1,67 @@
-const course : string = "Next level web development";
-console.log(course);
+{
+	const course: string = "Next level web development";
+	// console.log(course);
 
-// Basic Data Type
-// -----------------------------------------------------------------------
-// String
-// Implicit Data Type
-let firstName = "Nushrat";
+	// Basic Data Type
+	// -----------------------------------------------------------------------
+	// String
+	// Implicit Data Type
+	let firstName = "Nushrat";
 
-// Explicit Data Type
-let firstName2 : string = "Nushrat";
+	// Explicit Data Type
+	let firstName2: string = "Nushrat";
 
-// Number
-let roll : number = 1718048;
+	// Number
+	let roll: number = 1718048;
 
-// Boolean
-let isGraduated : boolean = true;
+	// Boolean
+	let isGraduated: boolean = true;
 
-// null
-let nothing : null = null;
+	// null
+	let nothing: null = null;
 
-// undefined
-let bolahoyni : undefined = undefined;
+	// undefined
+	let bolahoyni: undefined = undefined;
 
-// any
-let d;
+	// any
+	let d;
 
-d = 1;
-d = 'abc';
-d = true;
-d = undefined;
-d = null;
+	d = 1;
+	d = "abc";
+	d = true;
+	d = undefined;
+	d = null;
 
-console.log(d);
+	// console.log(d);
 
-// Array
-let friends : string[] = ['Hasib', 'Hasan'];
-let rolls : number[] = [1, 3];
+	// Array
+	let friends: string[] = ["Hasib", "Hasan"];
+	let rolls: number[] = [1, 3];
 
-// Tuple
-let ageName : [number, string, boolean] = [20, "Mr. X", true];
+	// Tuple
+	let ageName: [number, string, boolean] = [20, "Mr. X", true];
 
-// Object
-const user: {
-    company: "Programming Hero" //Literal Types. The value act as a type, and can't be changed
-    firstName: string;
-    middleName: string;
-    lastName?: string; //optional type: string | undefined
-    readonly Husband: string; // Access Modifier. Husband can't be changed
-} = {
-    company: "Programming Hero",
-    firstName: "Nushrat",
-    middleName: "Jahan",
-    Husband: "Hasan Ali"
+	// Object
+	const user: {
+		company: "Programming Hero"; //Literal Types. The value act as a type, and can't be changed
+		firstName: string;
+		middleName: string;
+		lastName?: string; //optional type: string | undefined
+		readonly Husband: string; // Access Modifier. Husband can't be changed
+	} = {
+		company: "Programming Hero",
+		firstName: "Nushrat",
+		middleName: "Jahan",
+		Husband: "Hasan Ali",
+	};
+
+	// Function
+	// Normal Function
+	function add(num1: number, num2: number): number {
+		return num1 + num2;
+	}
+	const addNumber = add(2, 3);
+    console.log(addNumber);
+
+
 }
-
-
-
-
-
